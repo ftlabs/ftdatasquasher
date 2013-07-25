d775459a74247468a61eb1b690c1a11e680a3d90
Matthew Andrews <matthew@andrews.eu.com>
2013-07-25 01:41:51 +0100

    100% coverage

--- test/tests/datasquasher.js
+++ test/tests/datasquasher.js
@@ -1,4 +1,5 @@
 var buster = require('buster');
+var squasher = require('../../coverage/lib/ftdatasquasher');
 
 buster.testCase('datasquasher', {
 
@@ -6,9 +7,7 @@ buster.testCase('datasquasher', {
 
 		// A base 64 encoded version of 'The quick brown fox jumps over the lazy dog'
 		var sample = "VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw==";
-
 		var expected = "噇桬䥈䘱慗乲䥇䩹戳摵䥇婶敃䉱摗ㅷ捹䉶摭噹䥈副婓䉳奘瀵䥇剶婷㴽";
-		var squasher = require('../../coverage/lib/ftdatasquasher');
 		var actual = squasher.compress(sample);
 
 		assert.equals(expected, actual);
@@ -16,6 +15,14 @@ buster.testCase('datasquasher', {
 		// And decompress again...
 		var actual = squasher.decompress(actual);
 		assert.equals(sample, actual);
+	},
+
+	"Edge case - check when string not even number of characters": function () {
+		var sample = "abc";
+		var expected = "慢挠";
+		var actual = squasher.compress(sample);
+
+		assert.equals(expected, actual);
 	}
 
 });
